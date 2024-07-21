e0b853ee7abc18de097eabeb36486754a967d41d
Theep21 <176223830+Theep21@users.noreply.github.com>
2024-07-21 21:37:40 +0700

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,25 @@
-touch index.py
+from flask import Flask, jsonify, request
+import yfinance as yf
+
+app = Flask(__name__)
+
+@app.route('/api/stock', methods=['GET'])
+def get_stock():
+    ticker = request.args.get('ticker')
+    stock = yf.Ticker(ticker)
+    stock_info = stock.info
+    todays_data = stock.history(period='1d')
+
+    stock_data = {
+        "symbol": stock_info.get("symbol"),
+        "companyName": stock_info.get("longName"),
+        "latestPrice": todays_data['Close'][0],
+        "change": stock_info.get("regularMarketChangePercent"),
+        "high": todays_data['High'][0],
+        "low": todays_data['Low'][0],
+        "logoURL": stock_info.get("logo_url")
+    }
+    return jsonify(stock_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
