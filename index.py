7c0dccd9bec14a2f6dbb4795fa6620eec2d7fbaa
Theep21 <176223830+Theep21@users.noreply.github.com>
2024-07-21 22:16:21 +0700

    Update index.py

--- index.py
+++ index.py
@@ -7,23 +7,19 @@ app = Flask(__name__)
 def get_stock():
     ticker = request.args.get('ticker')
     if not ticker:
-        return jsonify({'error': 'Ticker not provided'}), 400
-
+        return jsonify({'error': 'Ticker is required'}), 400
     stock = yf.Ticker(ticker)
     stock_info = stock.info
     todays_data = stock.history(period='1d')
 
-    if 'symbol' not in stock_info:
-        return jsonify({'error': 'Invalid ticker symbol'}), 400
-
     stock_data = {
-        "symbol": stock_info.get("symbol", "N/A"),
-        "companyName": stock_info.get("longName", "N/A"),
-        "latestPrice": todays_data['Close'][0] if not todays_data.empty else "N/A",
-        "change": stock_info.get("regularMarketChangePercent", "N/A"),
-        "high": todays_data['High'][0] if not todays_data.empty else "N/A",
-        "low": todays_data['Low'][0] if not todays_data.empty else "N/A",
-        "logoURL": stock_info.get("logo_url", "N/A")
+        "symbol": stock_info.get("symbol"),
+        "companyName": stock_info.get("longName"),
+        "latestPrice": todays_data['Close'][0],
+        "change": stock_info.get("regularMarketChangePercent"),
+        "high": todays_data['High'][0],
+        "low": todays_data['Low'][0],
+        "logoURL": stock_info.get("logo_url")
     }
 
     return jsonify(stock_data)
